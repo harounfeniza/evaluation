f65aa2aeba8903283990f47fb80b7a1dc9471de2
harounfeniza <62577819+harounfeniza@users.noreply.github.com>
2020-03-26 19:26:44 +0100

    Update test_fichier.py

--- test_fichier.py
+++ test_fichier.py
@@ -1,1 +1,11 @@
 from fichier import exemple
+from bubblesort import doctest
+import unittest
+
+class testbubblesort(unittest.TestCase):
+    def insererlist(self):
+        bubblesort.tries = list()
+
+ 
+if __name__ == '__main__':
+    unittest.main()
